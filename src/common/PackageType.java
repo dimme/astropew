9f1761d13be1debdfeed10ac446dab441b1073bb
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-06 14:26:49 +0000

    blargh
    git-svn-id: http://astropew.googlecode.com/svn/trunk@30 d889380e-1d37-11de-8b5c-13accb87f508

--- src/common/PackageType.java
+++ src/common/PackageType.java
@@ -7,4 +7,5 @@ public interface PackageType {
 	public static final byte OBJECT_POSITION = 34;
 	public static final byte INITIALIZER     = 35;
 	public static final byte PLAYER_JOINED   = 36;
+	public static final byte PLAYER_LEFT     = 37;
 }
