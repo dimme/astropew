1dd449d00ae194093ef960e88d8b5a0dc0fdaf9d
6a6f6e74656d616e6e656e <6a6f6e74656d616e6e656e@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-07 08:20:06 +0000

    Fredriks update....
    git-svn-id: http://astropew.googlecode.com/svn/trunk@55 d889380e-1d37-11de-8b5c-13accb87f508

--- src/common/PacketReaderThread.java
+++ src/common/PacketReaderThread.java
@@ -5,9 +5,14 @@
 
 package common;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.SocketAddress;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,28 +21,39 @@ import java.util.List;
 public class PacketReaderThread extends Thread {
 	private List<PacketObserver> observers;
 	private DatagramSocket socket;
+	private DatagramPacket readPacket;
 	private boolean running;
 
-	public void addPacketObserver( PacketObserver po ) {
+	public void addPacketObserver(PacketObserver po) {
 		observers.add(po);
 	}
 
-	public void notifyPacketObservers(byte[] data) {
+	public void removePacketObserver(PacketObserver po) {
+		observers.remove(po);
+	}
+
+	public void notifyPacketObservers(byte[] data, SocketAddress addr) {
 		for( PacketObserver po : observers ) {
-			po.packetReceived(data, null);
+			po.packetReceived(data, socket.getRemoteSocketAddress());
 		}
 	}
 
 	public PacketReaderThread(DatagramSocket socket) {
 		this.socket = socket;
 		observers = new LinkedList<PacketObserver>();
-
+		byte[] buff = new byte[Util.PACKET_SIZE];
+		readPacket = new DatagramPacket(buff, Util.PACKET_SIZE);
 	}
 
 	public void run() {
 		running = true;
 		while(running) {
-
+			try {
+				socket.receive(readPacket);
+				notifyPacketObservers(readPacket.getData(), readPacket.getSocketAddress());
+			} catch (IOException ex) {
+				Logger.getLogger(PacketReaderThread.class.getName()).log(Level.SEVERE, null, ex);
+			}
 		}
 	}
 
