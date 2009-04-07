ad6024fa6e61acbffdd605c6b06f0bdbce1ca866
qaunuckshin <qaunuckshin@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-07 07:17:22 +0000

    Cleaned up NetworkThread
    git-svn-id: http://astropew.googlecode.com/svn/trunk@41 d889380e-1d37-11de-8b5c-13accb87f508

--- src/client/NetworkThread.java
+++ src/client/NetworkThread.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,123 +23,137 @@ import java.util.logging.Logger;
  *
  * @author jonsturk
  */
-public class NetworkThread extends Thread{
-    private DatagramPacket rec;
-    private DatagramSocket sock;
-    private DatagramPacket send;
+public class NetworkThread extends Thread {
+	private DatagramPacket rec;
+	private DatagramSocket sock;
+	private DatagramPacket send;
 
-    private boolean connected;
-    private boolean running = true;
+	private boolean connected;
+	private boolean running = true;
+	
+	private String name; //TODO: Move to a better place.
+	private String hostname;
+	private int port;
 
-    private List<NetworkObserver> networkObservers;
+	private List<NetworkObserver> networkObservers;
 
-    public void addNetworkObserver(NetworkObserver nwo) {
-        networkObservers.add(nwo);
-    }
+	public void addNetworkObserver(NetworkObserver nwo) {
+		networkObservers.add(nwo);
+	}
 
-    public void notifyNetworkObservers(byte[] data) {
-        for( NetworkObserver nwo : networkObservers ) {
-            nwo.packetReceived(data);
-        }
-    }
+	public void notifyNetworkObservers(byte[] data) {
+		for( NetworkObserver nwo : networkObservers ) {
+			nwo.packetReceived(data);
+		}
+	}
 
-    public NetworkThread() {
-        setConnected(false);
-        networkObservers = new LinkedList<NetworkObserver>();
-    }
+	public NetworkThread(String name, String hostname, int port)  throws CatastrophicException {
+		this.name = name;
+		this.hostname = hostname;
+		this.port = port;
+		setConnected(false);
+		
+		networkObservers = new LinkedList<NetworkObserver>();
+		
+		rec = new DatagramPacket(new byte[65000], 65000);
+		
+		try {
+			sock = new DatagramSocket();
+		} catch (SocketException e) {
+			Logger.getLogger(NetworkThread.class.getName()).log(Level.SEVERE, null, e);
+			throw new CatastrophicException(e);
+		}
+	}
 
-    public NetworkThread(String name, String hostname, int port)  throws CatastrophicException {
-        this();
-        connect(name, hostname, port);
-    }
+	private void connect() throws CatastrophicException {
+		System.out.println("Sending 'want to join'");
+		new Exception().printStackTrace();
+		if( isConnected() ) {
+			return;
+		}
+		try {
+			InetAddress iaddr = InetAddress.getByName(hostname);
+			byte[] buf = name.getBytes();
+			send = new DatagramPacket(buf, buf.length, iaddr, port);
+			
+			sock.send(send);
+		} catch (Exception ex) {
+			Logger.getLogger(NetworkThread.class.getName()).log(Level.SEVERE, null, ex);
+			throw new CatastrophicException(ex);
+		} 
+	}
 
-    public void connect(String name, String hostname, int port) throws CatastrophicException {
-        if( isConnected() ) {
-            // Todo. Do something.
-        }
-        try {
-            InetAddress iaddr = InetAddress.getByName(hostname);
-            byte[] buf = name.getBytes();
-            send = new DatagramPacket(buf, buf.length, iaddr, port);
-            int size = 65000;
-            byte[] buf2 = new byte[size];
-            rec = new DatagramPacket(buf2, size);
-            sock = null;
-            sock = new DatagramSocket();
-            sock.send(send);
+	public void run() {
+		try {
+			connect();
+		} catch (CatastrophicException e) {
+			throw new RuntimeException(e);
+		}
+		while(isRunning()){
+			try {
+				sock.setSoTimeout(5000);
+				rec.setLength(rec.getData().length);
+				sock.receive(rec);
+				if( rec.getData()[0] == PackageType.INITIALIZER ){
+					notifyNetworkObservers(rec.getData());
+					sock.setSoTimeout(0);
+					setConnected(true);
+					break;
+				}
+			} catch (SocketTimeoutException e) {
+				// just loop and try again
+				System.out.println("Timeout, trying again..");
+				try {
+					connect();
+				} catch (CatastrophicException ex) {
+					throw new RuntimeException(ex);
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
 
-            int id = -1;
-            long randSeed = -1l;
-            sock.setSoTimeout(5000);
+		while (isRunning()) {
+			try {
+				rec.setLength(rec.getData().length); // reset length
+				sock.receive(rec);
+				byte[] bt = new byte[rec.getLength()];
+				System.arraycopy(rec.getData(), 0, bt, 0, bt.length);
+				notifyNetworkObservers(bt);
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		}
+	}
 
-            setConnected(true);
-        } catch (Exception ex) {
-            Logger.getLogger(NetworkThread.class.getName()).log(Level.SEVERE, null, ex);
-            throw new CatastrophicException(ex);
-        } 
-    }
+	/**
+	 * @return the connected
+	 */
+	public boolean isConnected() {
+		return connected;
+	}
 
-    public void run() {
-        while(isRunning()){
-            try {
-                sock.receive(rec);
-                if( rec.getData()[0] == PackageType.INITIALIZER ){
-                    notifyNetworkObservers(rec.getData());
-                    sock.setSoTimeout(0);
-                    break;
-                }
-            } catch (SocketTimeoutException e) {
-                
-            } catch (IOException e ) {
-                try {
-                    sock.send(send);
-                } catch (IOException e1) {
-                    throw new RuntimeException(e1);
-                }
-                throw new RuntimeException(e);
-            }
-        }
+	/**
+	 * @param connected the connected to set
+	 */
+	public void setConnected(boolean connected) {
+		this.connected = connected;
+	}
 
-        while (isRunning()) {
-            try {
-                sock.receive(rec);
-                byte[] bt = new byte[rec.getLength()];
-                System.arraycopy(rec.getData(), 0, bt, 0, bt.length);
-                notifyNetworkObservers(bt);
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
+	/**
+	 * @return the running
+	 */
+	public boolean isRunning() {
+		return running;
+	}
 
-    /**
-     * @return the connected
-     */
-    public boolean isConnected() {
-        return connected;
-    }
-
-    /**
-     * @param connected the connected to set
-     */
-    public void setConnected(boolean connected) {
-        this.connected = connected;
-    }
-
-    /**
-     * @return the running
-     */
-    public boolean isRunning() {
-        return running;
-    }
-
-    /**
-     * @param running the running to set
-     */
-    public void setRunning(boolean running) {
-        this.running = running;
-        if( running == false && sock != null && sock.isConnected()) {
-            sock.close();
-        }
-    }
+	/**
+	 * @param running the running to set
+	 */
+	public void setRunning(boolean running) {
+		this.running = running;
+		if( running == false && sock != null && sock.isConnected()) {
+			sock.close();
+		}
+	}
 }
--- src/common/PackageType.java
+++ src/common/PackageType.java
@@ -2,10 +2,11 @@ package common;
 
 public interface PackageType {
 	
-	public static final byte MESSAGE         = 32;
-	public static final byte PLAYER_POSITION = 33;
-	public static final byte OBJECT_POSITION = 34;
-	public static final byte INITIALIZER     = 35;
-	public static final byte PLAYER_JOINED   = 36;
-	public static final byte PLAYER_LEFT     = 37;
+	public static final byte MESSAGE         = 32; // A message to be shown.
+	public static final byte PLAYER_POSITION = 33; // Player position/direction
+	public static final byte OBJECT_POSITION = 34; // Object ---||---
+	public static final byte INITIALIZER     = 35; // Sent to joining client
+	public static final byte PLAYER_JOINED   = 36; // Sends a joined player's id and name to other players
+	public static final byte PLAYER_LEFT     = 37; // Informs players that someone left
+	public static final byte LEAVING         = 38; // Informs server that client is leaving
 }
