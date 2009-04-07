be01bec412f8109105ad9df48efd486c92c96853
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-07 08:44:13 +0000

    javadoc'ed PackageType.java
    git-svn-id: http://astropew.googlecode.com/svn/trunk@60 d889380e-1d37-11de-8b5c-13accb87f508

--- src/common/PackageType.java
+++ src/common/PackageType.java
@@ -2,11 +2,38 @@ package common;
 
 public interface PackageType {
 	
-	public static final byte MESSAGE		 = 32; // A message to be shown.
-	public static final byte PLAYER_POSITION = 33; // Player position/direction
-	public static final byte OBJECT_POSITION = 34; // Object ---||---
+	/**
+	 * Some kind of message to be shown.
+	 */
+	public static final byte MESSAGE		 = 32;
+	
+	/**
+	 * Player position and direction
+	 */
+	public static final byte PLAYER_POSITION = 33;
+	
+	/**
+	 * Object position and direction
+	 */
+	public static final byte OBJECT_POSITION = 34;
+	
+	/**
+	 * Sent to joining client to acknowledge their joining
+	 */
 	public static final byte INITIALIZER	 = 35; // Sent to joining client
-	public static final byte PLAYER_JOINED   = 36; // Sends a joined player's id and name to other players
-	public static final byte PLAYER_LEFT	 = 37; // Informs players that someone left
-	public static final byte LEAVING		 = 38; // Informs server that client is leaving
+	
+	/**
+	 * Sends info about a joining player to other players
+	 */
+	public static final byte PLAYER_JOINED   = 36;
+	
+	/**
+	 * Informs players that someone left.
+	 */
+	public static final byte PLAYER_LEFT	 = 37;
+	
+	/**
+	 * Informs server that a client is leaving.
+	 */
+	public static final byte LEAVING		 = 38; 
 }
