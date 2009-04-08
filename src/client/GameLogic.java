17d59c5142a1e11d27d60947f0c814e1b0adfa9e
qaunuckshin <qaunuckshin@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-08 09:32:10 +0000

    start of GameLogic on client-side
    git-svn-id: http://astropew.googlecode.com/svn/trunk@84 d889380e-1d37-11de-8b5c-13accb87f508

---
+++ src/client/GameLogic.java
@@ -0,0 +1,12 @@
+package client;
+
+import client.world.World;
+
+public class GameLogic extends common.GameLogic 
+{
+
+	public GameLogic(World world) {
+		super(world);
+	}
+	
+}
---
+++ src/client/world/World.java
@@ -0,0 +1,18 @@
+package client.world;
+
+import com.jme.app.SimpleGame;
+import com.jme.scene.Spatial;
+
+public class World extends SimpleGame implements common.world.World{
+
+	protected void simpleInitGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void attachChild(Spatial child) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
