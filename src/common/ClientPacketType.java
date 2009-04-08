0efff3f6d7f2dde6b6d074489ca49858dda1e4f7
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-08 09:40:52 +0000

    ClientPacketType changE?
    git-svn-id: http://astropew.googlecode.com/svn/trunk@87 d889380e-1d37-11de-8b5c-13accb87f508

--- src/common/ClientPacketType.java
+++ src/common/ClientPacketType.java
@@ -4,12 +4,19 @@ public interface ClientPacketType {
 	
 	/**
 	 * Informs server that a client is leaving.
+	 * A LEAVING packet contains:
+	 * ClientPacketType - byte - 1 byte
 	 */
 	public static final byte LEAVING		 = 64; 
 	
 	
 	/**
 	 * Informs server where we are headed
+	 * A PLAYER_MOVEMENT packer contains:
+	 * ClientPacketType - byte - 1 byte
+	 * Time - long - 8 byte
+	 * dir - Vector3f - 12 byte
+	 * ort - Vector3f - 12 byte
 	 */
 	public static final byte PLAYER_MOVEMENT = 65;
 
