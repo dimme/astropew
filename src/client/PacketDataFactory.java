8aee8a70562c81cbd8e005c7e172edbcc4c4322f
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-14 09:21:32 +0000

    Added OffsetAndSizeConstants interface.
    git-svn-id: http://astropew.googlecode.com/svn/trunk@102 d889380e-1d37-11de-8b5c-13accb87f508

--- src/client/PacketDataFactory.java
+++ src/client/PacketDataFactory.java
@@ -1,18 +1,19 @@
 package client;
 
 import common.ClientPacketType;
+import common.OffsetAndSizeConstants;
 import common.Util;
 import common.world.Ship;
 
 public class PacketDataFactory {
 
 	public static byte[] createMovement(long time, Ship s){
-		byte[] b = new byte[34]; //1 + 1 + 8 + 2*3*4
-		b[0] = ClientPacketType.PLAYER_MOVEMENT;
-		b[1] = 0; //Seq number
-		Util.put(time, b, 2);
-		Util.put(s.getLocalTranslation(), b, 10);
-		Util.put(s.getMovement(), b, 22);
+		byte[] b = new byte[OffsetAndSizeConstants.PLAYER_MOVEMENT_SIZE];
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ClientPacketType.PLAYER_MOVEMENT;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		Util.put(time, b, OffsetAndSizeConstants.PLAYER_MOVEMENT_TIME_OFFSET);
+		Util.put(s.getLocalTranslation(), b, OffsetAndSizeConstants.PLAYER_MOVEMENT_ORT_OFFSET);
+		Util.put(s.getMovement(), b,OffsetAndSizeConstants.PLAYER_MOVEMENT_DIR_OFFSET);
 		return b;
 	}
 }
--- src/server/PacketDataFactory.java
+++ src/server/PacketDataFactory.java
@@ -1,5 +1,6 @@
 package server;
 
+import common.OffsetAndSizeConstants;
 import common.ServerPacketType;
 import common.Util;
 import common.world.Ship;
@@ -10,59 +11,59 @@ public class PacketDataFactory {
 		
 		byte[] sb = str.getBytes();
 		
-		byte[] b = new byte[3 + sb.length]; //1+1+1+name
+		byte[] b = new byte[OffsetAndSizeConstants.MESSAGE_STRING_OFFSET + sb.length];
 		
-		b[0] = ServerPacketType.MESSAGE;
-		b[1] = 0; //Seq nbr
-		b[2] = msgtype;
-		Util.put(sb, b, 3);
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ServerPacketType.MESSAGE;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		b[OffsetAndSizeConstants.MESSAGE_MESSAGE_TYPE_OFFSET] = msgtype;
+		Util.put(sb, b, OffsetAndSizeConstants.MESSAGE_STRING_OFFSET);
 		
 		return b;
 	}
 	
 	public static byte[] createInitializer(long worldseed, int id, String name) {
 		byte[] namebytes = name.getBytes();
-		byte[] b = new byte[14 + namebytes.length];//1 + 1 + 8 + 4 + l
+		byte[] b = new byte[OffsetAndSizeConstants.INITIALIZER_STRING_OFFSET + namebytes.length];
 		
-		b[0] = ServerPacketType.INITIALIZER;
-		b[1] = 0; //Seq nbr
-		Util.put(worldseed, b, 2);
-		Util.put(id, b, 10);
-		Util.put(namebytes, b, 14);
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ServerPacketType.INITIALIZER;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		Util.put(worldseed, b, OffsetAndSizeConstants.INITIALIZER_RANDOM_SEED_OFFSET);
+		Util.put(id, b, OffsetAndSizeConstants.INITIALIZER_ID_OFFSET);
+		Util.put(namebytes, b, OffsetAndSizeConstants.INITIALIZER_STRING_OFFSET);
 		
 		return b;
 	}
 	
 	public static byte[] createPlayerJoined(int id, String name) {
 		byte[] sb = name.getBytes();
-		byte[] b = new byte[6 + sb.length]; // 1 + 1 + 4 + l
+		byte[] b = new byte[OffsetAndSizeConstants.PLAYER_JOINED_STRING_OFFSET + sb.length];
 		
-		b[0] = ServerPacketType.PLAYER_JOINED;
-		b[1] = 0; //Seq nbr
-		Util.put(id,b,2);
-		Util.put(sb,b,6);
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ServerPacketType.PLAYER_JOINED;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		Util.put(id,b,OffsetAndSizeConstants.PLAYER_JOINED_ID_OFFSET);
+		Util.put(sb,b,OffsetAndSizeConstants.PLAYER_JOINED_STRING_OFFSET);
 		
 		return b;
 	}
 	
 	public static byte[] createPlayerLeft(int id) {
-		byte[] b = new byte[6]; // 1 + 1 + 4
-		b[0] = ServerPacketType.PLAYER_LEFT;
-		b[1] = 0; //Seq nbr
-		Util.put(id, b, 2);
+		byte[] b = new byte[OffsetAndSizeConstants.PLAYER_LEFT_SIZE];
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ServerPacketType.PLAYER_LEFT;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		Util.put(id, b, OffsetAndSizeConstants.PLAYER_LEFT_ID_OFFSET);
 		return b;
 	}
 
 	public static byte[] createPosition(long time, Ship s) {
-		byte[] b = new byte[50]; //1 + 1 + 8 +4 + 3*3*4
+		byte[] b = new byte[OffsetAndSizeConstants.PLAYER_POSITION_SIZE];
 		
-		b[0] = ServerPacketType.PLAYER_POSITION;
-		b[1] = 0; //Seq nbr
-		Util.put(time, b, 2);
-		Util.put(s.getOwner().getID(), b, 10);
-		Util.put(s.getLocalTranslation(), b, 14);
-		Util.put(s.getLocalRotation().getRotationColumn(2), b, 26);
-		Util.put(s.getMovement(), b, 38);
+		b[OffsetAndSizeConstants.PACKET_TYPE_OFFSET] = ServerPacketType.PLAYER_POSITION;
+		b[OffsetAndSizeConstants.SEQUENCE_NUMBER_OFFSET] = 0;
+		Util.put(time, b, OffsetAndSizeConstants.PLAYER_POSITION_TIME_OFFSET);
+		Util.put(s.getOwner().getID(), b, OffsetAndSizeConstants.PLAYER_POSITION_ID_OFFSET);
+		Util.put(s.getLocalTranslation(), b, OffsetAndSizeConstants.PLAYER_POSITION_POS_OFFSET);
+		Util.put(s.getLocalRotation().getRotationColumn(2), b, OffsetAndSizeConstants.PLAYER_POSITION_DIR_OFFSET);
+		Util.put(s.getMovement(), b, OffsetAndSizeConstants.PLAYER_POSITION_ORT_OFFSET);
 		
 		return b;
 	}
