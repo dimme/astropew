f677fe68f58804990fac8060b5ef65928dd38613
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-14 08:42:31 +0000

    Updated client.PackedDataFactory with seq number space.
    git-svn-id: http://astropew.googlecode.com/svn/trunk@99 d889380e-1d37-11de-8b5c-13accb87f508

--- src/client/PacketDataFactory.java
+++ src/client/PacketDataFactory.java
@@ -7,11 +7,12 @@ import common.world.Ship;
 public class PacketDataFactory {
 
 	public static byte[] createMovement(long time, Ship s){
-		byte[] b = new byte[1 + 8 + 2*3*4];
+		byte[] b = new byte[33]; //1 + 8 + 2*3*4
 		b[0] = ClientPacketType.PLAYER_MOVEMENT;
-		Util.put(time, b, 1);
-		Util.put(s.getLocalTranslation(), b, 9);
-		Util.put(s.getMovement(), b, 21);
+		b[1] = 0; //Seq number
+		Util.put(time, b, 2);
+		Util.put(s.getLocalTranslation(), b, 10);
+		Util.put(s.getMovement(), b, 22);
 		return b;
 	}
 }
