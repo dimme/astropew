6c453f1468e8a5fc44e32d09226694facc69ce72
FredrikPersson <FredrikPersson@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-15 12:37:30 +0000

    Cleaned up ConsoleNetworkObserver a bit.
    git-svn-id: http://astropew.googlecode.com/svn/trunk@133 d889380e-1d37-11de-8b5c-13accb87f508

--- src/client/ConsoleNetworkObserver.java
+++ src/client/ConsoleNetworkObserver.java
@@ -15,69 +15,41 @@ public class ConsoleNetworkObserver implements PacketObserver {
 
 	public boolean packetReceived(byte[] data, SocketAddress saddr) {
 		final byte ptype = Util.packetType(data);
+		
 		if (ptype == ServerPacketType.INITIALIZER) {
-			final String name = new String(data,
+			final String name = new String(data,  
 					OffsetConstants.INITIALIZER_STRING_OFFSET, data.length
-							- OffsetConstants.INITIALIZER_STRING_OFFSET);
-			final int id = Util.getInt(data,
-					OffsetConstants.INITIALIZER_ID_OFFSET);
-			final long randSeed = Util.getLong(data,
-					OffsetConstants.INITIALIZER_RANDOM_SEED_OFFSET);
-			System.out.println("Established contact with server. Got id: " + id
-					+ ". Got name: " + name + ". Got seed: " + randSeed);
+				   -OffsetConstants.INITIALIZER_STRING_OFFSET);
+			final int id = Util.getInt(data, OffsetConstants.INITIALIZER_ID_OFFSET);
+			final long randSeed = Util.getLong(data, OffsetConstants.INITIALIZER_RANDOM_SEED_OFFSET);
+			System.out.println(	"Established contact with server. Got id: " + id +
+								". Got name: " + name + ". Got seed: " + randSeed);
 		} else if (ptype == ServerPacketType.PLAYER_LEFT) {
-			final int lid = Util.getInt(data,
-					OffsetConstants.PLAYER_LEFT_ID_OFFSET);
+			final int lid = Util.getInt(data, OffsetConstants.PLAYER_LEFT_ID_OFFSET);
 			System.out.println("Player Left. ID = " + lid);
 		} else if (ptype == ServerPacketType.PLAYER_JOINED) {
-			final int nid = Util.getInt(data,
-					OffsetConstants.PLAYER_JOINED_ID_OFFSET);
-			final byte[] bt = new byte[data.length
-					- OffsetConstants.PLAYER_JOINED_STRING_OFFSET];
-			System.arraycopy(data, OffsetConstants.PLAYER_JOINED_STRING_OFFSET,
-					bt, 0, bt.length);
-			System.out.println("Player Joined. ID = " + nid + " Name = "
-					+ new String(bt));
+			final int nid = Util.getInt(data, OffsetConstants.PLAYER_JOINED_ID_OFFSET);
+			final byte[] bt = new byte[data.length - OffsetConstants.PLAYER_JOINED_STRING_OFFSET];
+			System.arraycopy(data, OffsetConstants.PLAYER_JOINED_STRING_OFFSET, bt, 0, bt.length);
+			System.out.println("Player Joined. ID = " + nid + " Name = " + new String(bt));
 		} else if (ptype == ServerPacketType.PLAYERS_INFO) {
 			String s = "";
 			for (int i = 2; i < data.length;) {
-				i += 4;
-				final byte tmp = data[i];
-				i++;
-				final String name = new String(data, i, tmp);
-				i += tmp;
+				final byte tmp = data[i+4];
+				final String name = new String(data, i+5, tmp);
+				i += tmp + 5;
 				s += name + ", ";
 			}
 			System.out.println("Added players: " + s);
 		} else if (ptype == ServerPacketType.PLAYER_POSITIONS) {
-			/*
-			 * System.out.println("Recieved a player_position packet");
-			 * System.out.println("The time was: "+Util.getLong(data,
-			 * OffsetAndSizeConstants.PLAYER_POSITION_TICK_OFFSET));
-			 * System.out.println("The player was: "+Util.getInt(data,
-			 * OffsetAndSizeConstants.PLAYER_POSITION_ID_OFFSET)); Vector3f v =
-			 * new Vector3f(); Util.getVector3f(data,
-			 * OffsetAndSizeConstants.PLAYER_POSITION_POS_OFFSET, v);
-			 * System.out.println(v); Util.getVector3f(data,
-			 * OffsetAndSizeConstants.PLAYER_POSITION_DIR_OFFSET, v);
-			 * System.out.println(v); Util.getVector3f(data,
-			 * OffsetAndSizeConstants.PLAYER_POSITION_ORT_OFFSET, v);
-			 * System.out.println(v);
-			 */
+			//No need for much here since we see them move
 		} else if (ptype == ServerPacketType.MESSAGE) {
-			System.out
-					.println("Recieved msg: "
-							+ new String(
-									data,
-									OffsetConstants.MESSAGE_STRING_OFFSET,
-									data.length
-											- OffsetConstants.MESSAGE_STRING_OFFSET));
+			System.out.println("Recieved msg: " + new String(data,
+									OffsetConstants.MESSAGE_STRING_OFFSET, data.length
+								   -OffsetConstants.MESSAGE_STRING_OFFSET));
 		} else {
-			/*
-			 * System.out.println("Unhandled packet type: " + unmaskedData +
-			 * ", length: " + data.length); System.out.println("\t" + "Data: " +
-			 * hex(data));
-			 */
+			 System.out.println("Unhandled packet type: " + ptype +
+			 ", length: " + data.length + ". Data: "+Util.hex(data)); 
 		}
 		return false;
 	}
