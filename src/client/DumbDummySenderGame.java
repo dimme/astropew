ab9067a0491797e285d7185b89c34644b67a953f
qaunuckshin <qaunuckshin@d889380e-1d37-11de-8b5c-13accb87f508>
2009-04-16 08:07:20 +0000

    automated dummysendergame
    git-svn-id: http://astropew.googlecode.com/svn/trunk@142 d889380e-1d37-11de-8b5c-13accb87f508

--- src/client/DumbDummySenderGame.java
+++ src/client/DumbDummySenderGame.java
@@ -4,6 +4,7 @@ import java.util.concurrent.RejectedExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 
 import common.world.Ship;
@@ -12,10 +13,19 @@ public class DumbDummySenderGame extends Game {
 
 	private long frameTime = 0;
 	private float ticklength;
+	private Snapshot[] snaps;
 	
 	public DumbDummySenderGame(GameClient gc) {
 		super(gc);
-		
+		snaps = new Snapshot[20];
+		double ang;
+		for (int i=0; i<snaps.length; i++) {
+			ang = i * 8 * Math.PI / snaps.length;
+			snaps[i] = new Snapshot(
+					new Vector3f(0, 4*(float)Math.sin(ang), -(float)ang),
+					new Quaternion(),
+					new Vector3f());
+		}
 	}
 	
 	protected void simpleInitGame() {
@@ -32,9 +42,16 @@ public class DumbDummySenderGame extends Game {
 		common.Player self = logic.getSelf();
 		if(self != null) {
 			Ship s = self.getShip();
-			Vector3f mov = new Vector3f(Vector3f.UNIT_Z);
-			mov = mov.mult(-delta);
-			s.getLocalTranslation().addLocal(mov);
+			
+			long time = System.currentTimeMillis();
+			Snapshot oldshot = snaps[(int)(time/1000)%snaps.length];
+			Snapshot newshot = snaps[(int)(time/1000+1)%snaps.length];
+			
+			Snapshot shot = oldshot.interpolate(newshot, time, 1000);
+			
+			s.setLocalTranslation(shot.pos);
+			s.setLocalRotation(shot.ort);
+			s.setMovement(shot.dir, Long.MAX_VALUE);
 		
 			byte[] data = PacketDataFactory.createPlayerUpdate(System.currentTimeMillis(), s);
 			try {
@@ -45,4 +62,24 @@ public class DumbDummySenderGame extends Game {
 		}
 	}
 
+	private class Snapshot {
+		Vector3f pos;
+		Quaternion ort;
+		Vector3f dir;
+		
+		public Snapshot(Vector3f pos, Quaternion ort, Vector3f dir) {
+			super();
+			this.dir = dir;
+			this.ort = ort;
+			this.pos = pos;
+		}
+
+		public Snapshot interpolate(Snapshot newshot, long time, long timediff) {
+			time = time % timediff;
+			Vector3f p = pos.add( newshot.pos.subtract(pos).mult(time/(float)timediff) );
+			
+			return new Snapshot(p, ort, dir);
+		}
+	}
+	
 }
